c9f423d342e0c6b70439230aa4ccbd9cf62554ce
coreydevdfw <corey.spence.shs@gmail.com>
2026-07-16 19:10:11 -0500

    Bump service worker CACHE_VERSION to v3 for today's index.html changes

--- service-worker.js
+++ service-worker.js
@@ -2,7 +2,7 @@
 // Lives in the repo root, alongside index.html.
 //
 // Bump CACHE_VERSION any time you change ANY file listed in APP_SHELL below.
-const CACHE_VERSION = 'v2';
+const CACHE_VERSION = 'v3';
 const CACHE_NAME = `ground-network-${CACHE_VERSION}`;
 
 const APP_SHELL = [
