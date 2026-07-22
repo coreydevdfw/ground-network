ba4563a84d6484ed991a2b453e878645796ac205
coreydevdfw <corey.spence.shs@gmail.com>
2026-07-22 13:35:59 -0500

    Bump CACHE_VERSION to 'v9' for privacy.html link/legal-note changes

--- service-worker.js
+++ service-worker.js
@@ -2,7 +2,7 @@
 // Lives in the repo root, alongside index.html.
 //
 // Bump CACHE_VERSION any time you change ANY file listed in APP_SHELL below.
-const CACHE_VERSION = 'v8';
+const CACHE_VERSION = 'v9';
 const CACHE_NAME = `ground-network-${CACHE_VERSION}`;
 
 const APP_SHELL = [
